080a68416cb31b30d17d09718c2ffd9b13b63a38
Jeffrey Shu <jshu05@gmail.com>
2024-01-10 14:15:16 -0800

    Add Financial Statement Class

--- transaction.py
+++ transaction.py
@@ -1,9 +1,31 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import List
 
 
 @dataclass
 class Transaction:
-    date_and_time: datetime
     merchant_name_description: str
     amount: float
+    # date_and_time:
+
+
+# class Transaction:
+#     def __init__(self, merchant_name_description, amount):
+#         self.merchant_name_description = merchant_name_description
+#         self.amount = amount
+
+
+class BankStatement:
+    def __init__(self):
+        self.transactions: List[Transaction] = []
+
+    def import_statement(self, csv_filepath: str):
+        with open(csv_filepath) as statements:
+            for transaction in statements:
+                self.transactions.append(transaction)
+
+
+# bank_statement = BankStatement()
+# bank_statement.import_statement("./bankstatement.csv")
+# print("Number of transactions: {}".format(len(bank_statement.transactions)))
