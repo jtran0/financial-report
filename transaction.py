83a7c0001f27b7f84cbd2360facb75e7ed2cb7a5
Jeffrey Shu <jshu05@gmail.com>
2024-01-12 16:40:18 -0800

    Add Amex, Citi, and Wells Fargo Class

--- transaction.py
+++ transaction.py
@@ -5,11 +5,12 @@ from typing import List
 
 @dataclass
 class Transaction:
-    def __init__(self, posting_date, description, amount, balance):
+    def __init__(self, posting_date, description, amount, balance, category):
         self.posting_date = posting_date
         self.description = description
         self.amount = amount
         self.balance = balance
+        self.category = category
 
 
 class FinancialReport:
@@ -43,4 +44,38 @@ class Chase(FinancialReport):
                 transaction.pop(1)  # Remove Post Date
                 transaction.pop(-1)  # Remove White Space
                 transaction.pop(-2)  # Remove Type
+        return self.statement
+
+
+class AmericanExpress(FinancialReport):
+    def __init__(self):
+        super().__init__()
+
+    def format_amex(self):
+        for transactions in self.statement:
+            for transaction in transactions:
+                del transaction[3:-1]  # Remove clutter
+        return self.statement
+
+
+class Citi(FinancialReport):
+    def __init__(self):
+        super().__init__()
+
+    def format_citi(self):
+        for transactions in self.statement:
+            for transaction in transactions:
+                del transaction[0]  # Remove Status
+                del transaction[-1]  # Remove Member Name
+        return self.statement
+
+
+class WellsFargo(FinancialReport):
+    def __init__(self):
+        super().__init__()
+
+    def format_wells(self):
+        for transactions in self.statement:
+            for transaction in transactions:
+                del transaction[2:4]  # Remove Empty Space
         return self.statement
