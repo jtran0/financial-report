a0aee79288608919fbac2e068e9546c72bb31eed
Jeffrey Shu <jshu05@gmail.com>
2024-01-30 12:08:36 -0800

    Add Chase Checking and Credit Card Test

--- test_account.py
+++ test_account.py
@@ -22,3 +22,16 @@ class TestAccount:
         report.filter_transations()
         assert report.total_expense == -981
         assert report.ignore_payment == 1300
+
+    def test_chase_checking_and_credit(self):
+        report = ReportGenerator()
+        chase_transaction_parser = ChaseTransactionParser()
+        chase_transaction_parser.parse_statement("test_files/Test_ChaseCheckingAcc.csv")
+        chase_transaction_parser.parse_statement(
+            "test_files/Chase_CreditCard_Test_Sample.csv"
+        )
+        report.import_transactions(chase_transaction_parser.statement)
+        report.filter_transations()
+        assert report.ignore_payment == -5700.00
+        assert report.total_income == 2300.00
+        assert report.total_expense == -981
